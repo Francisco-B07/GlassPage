a4ccf630830477a77f83ab1d9626b298654ac202
Joaquín Agüero <110200358+joatato@users.noreply.github.com>
2023-06-16 13:55:22 -0300

    Correccion para usar glitch

--- src/app.js
+++ src/app.js
@@ -1,49 +1,47 @@
 import path from 'path';
 import express from 'express';
-import { engine } from 'express-handlebars'
+import { engine } from 'express-handlebars';
 import { __dirname } from './utils/utils.js';
+import { config } from './config/config.js';
 
-const PORT = 3000;
+const PORT = config.app.port || 3000;
 
 const app = express();
 
 app.engine('handlebars', engine({
-    runtimeOptions: {
-        allowProtoPropertiesByDefault: true,
-        allowProtoMethodsByDefault: true
-    }
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true
+  }
 }));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '../views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, '../public')));
 
-
 app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).render('home', {
-        style: "./css/home.css"
-    });
-})
+  res.render('home', {
+    style: "/css/home.css"
+  });
+});
+
 app.get('/products', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).render('products', {
-        style: "./css/products.css",
-        header:true
-    });
-})
+  res.render('products', {
+    style: "/css/products.css",
+    header: true
+  });
+});
+
 app.get('/contact', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).render('contact', {
-        style: "./css/contact.css"
-    });
-})
+  res.render('contact', {
+    style: "/css/contact.css"
+  });
+});
 
 const server = app.listen(PORT, () => {
-    console.log(`Server escuchando en puerto ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 
 server.on('error', (error) => console.log(error));
